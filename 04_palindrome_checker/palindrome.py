2caa2c2ac9d590e45e94e7b4f11866f0b5abe27f
Manasa Umashankar <237534003+MNSinfoSEC@users.noreply.github.com>
2026-08-20 23:23:09 +0530

    Add palindrome checker function in palindrome.py

---
+++ 04_palindrome_checker/palindrome.py
@@ -0,0 +1,14 @@
+# Palindrome Checker
+# Python Foundation - Project 04
+
+def is_palindrome(text):
+    text = text.lower()
+    return text == text[::-1]
+
+
+word = input("Enter a word: ")
+
+if is_palindrome(word):
+    print("Palindrome! ✅")
+else:
+    print("Not a palindrome. ❌")
